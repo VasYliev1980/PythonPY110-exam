8fe91e630bf3ba88029fe7599471dec743d3cff6
VasYliev1980 <Yura.Vasilev1980@yandex.ru>
2022-12-23 20:41:40 +0300

    add.validate_isbn_13.py

--- main.py
+++ main.py
@@ -7,57 +7,65 @@ import json
 
 fake = Faker(locale="ru_RU")
 
-def check_field_length(field_name: str, max_length: int):   # Фабрика декораторов, проверяющая ограничение длинны max_lengthсимволов для поля field_name
+
+def check_field_length(field_name: str, max_length: int):  # Фабрика декораторов, проверяющая ограничение длинны max_length символов для поля field_name
     def decorator(f):
         def wrapper(*args, **kwargs):
-            res = f(*args, **kwargs):
+            res = f(*args, **kwargs)
             if len(res) > max_length:
-                raise ValueError(f"Длинна поля {field_name} ghtdsiftn {max_length} символов")
+                raise ValueError(f"Длинна поля {field_name} превышает {max_length} символов")
             return res
         return wrapper
     return decorator
 
 
-@check_field_length("title", 25)   # С помощью фабрики декоратора задаем ограничение длинны в 25 символов для поля title
-def get_title() -> str: # Функция получения поля title
+@check_field_length("title", 25)  # С помощью фабрики декоратора задаем ограничение длинны в 25 символов для поля title
+def get_title() -> str:  # Функция получения поля title
     return linecache.getline("books.txt", random.randint(1, 5)).strip()
 
 
-def get_year() -> int: # Функция получения поля Year
+def get_year() -> int:  # Функция получения поля year
     return random.randint(1900, datetime.datetime.now().year)
 
-def get_pages() -> int: # функция получения поля pages
-    return random.randint(0,10000)
+
+def get_pages() -> int:  # Функция получения поля pages
+    return random.randint(0, 10000)
+
 
 def get_isbn13() -> str:  # Функция получения поля isbn13
     return fake.isbn13()
 
-def get_raiting() -> float: # Функция получения поля raiting
+
+def get_rating() -> float:  # Функция получения поля rating
     return random.uniform(0, 5)
 
-def get_price() -> float: # Функция получения поля price
+
+def get_price() -> float:  # Функция получения поля price
     return random.uniform(0, 10000)
 
-def get_authors() -> list[str]: # Функция получения поля authors
+
+def get_authors() -> list[str]:  # Функция получения поля authors
     return list(fake.name() for _ in range(random.randint(1, 3)))
 
-def gen_dir(num:int = 1) -> dir: # Функция-генератор словарей
+
+def gen_dir(num: int = 1) -> dir:  # Функция-генератор словарей
     pk = num
     while True:
         yield {
             "model": conf.MODEL,
-            "pk":pk,
-            "fields":{
+            "pk": pk,
+            "fields": {
                 "title": get_title(),
                 "year": get_year(),
                 "pages": get_pages(),
                 "isbn13": get_isbn13(),
-                "raiting": get_raiting(),
+                "rating": get_rating(),
                 "price": get_price(),
                 "authors": get_authors()
             }
         }
         pk += 1
+
 
 def main() -> None:
     file_name = "dir.json"
@@ -67,7 +75,6 @@ def main() -> None:
     with open(file_name, "w") as fout:
         fout.write(json.dumps(list(next(gen) for _ in range(100)), indent=4, ensure_ascii=False))
 
-
     # Проверка записанных в файл словарей
     with open(file_name) as fin:
         for s in fin:
@@ -76,3 +83,5 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+
--- validate_isbn_13.py
+++ validate_isbn_13.py
@@ -1,6 +1,7 @@
 import re
-def check_isbn13(isbn: str) -> bool: # Функция проверки строки на соответствие формату ISBN-13
-    pattern = r"97[89][- ]\d{1,6}[- ][\d,X]$"
+
+def check_isbn13(isbn: str) -> bool:  # Функция проверки строки на соответствие формату ISBN-13
+    pattern = r"97[89][- ]\d{1,5}[- ]\d{1,7}[- ]\d{1,6}[- ][\d,X]$"
     if re.match(pattern, isbn) is not None:
         return True
     return False
