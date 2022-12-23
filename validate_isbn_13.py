35599a1c1dd294b845fb5b04a5a61d9c389c00fd
VasYliev1980 <Yura.Vasilev1980@yandex.ru>
2022-12-23 20:06:45 +0300

    add.validate_isbn_13.py

--- validate_isbn_13.py
+++ validate_isbn_13.py
@@ -1,2 +1,6 @@
 import re
 def check_isbn13(isbn: str) -> bool: # Функция проверки строки на соответствие формату ISBN-13
+    pattern = r"97[89][- ]\d{1,6}[- ][\d,X]$"
+    if re.match(pattern, isbn) is not None:
+        return True
+    return False
