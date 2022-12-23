31f59ee16f9d95af7ee240ff16b13ac5668f7c17
VasYliev1980 <Yura.Vasilev1980@yandex.ru>
2022-12-23 19:22:03 +0300

    add.main.py

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ import json
 
 fake = Faker(locale="ru_RU")
 
-def check_field_length(field_name: str, max_length: int):
+def check_field_length(field_name: str, max_length: int):   # Фабрика декораторов, проверяющая ограничение длинны max_lengthсимволов для поля field_name
     def decorator(f):
         def wrapper(*args, **kwargs):
             res = f(*args, **kwargs):
-            if len(res) > max_length
+            if len(res) > max_length:
+                raise ValueError(f"Длинна поля {field_name} ghtdsiftn {max_length} символов")
+            return res
+        return wrapper
+    return decorator
+
+
+@check_field_length("title", 25)   # С помощью фабрики декоратора задаем ограничение длинны в 25 символов для поля title
+def get_title() -> str: # Функция получения поля title
+    return linecache.getline("books.txt", random.randint(1, 5)).strip()
+
+
+
