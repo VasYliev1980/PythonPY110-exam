c0cc22a08dadef60f01028c0e23a82c13f1248e7
VasYliev1980 <Yura.Vasilev1980@yandex.ru>
2022-12-23 19:07:21 +0300

    add.main.py

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ import linecache
 import json
 
 fake = Faker(locale="ru_RU")
+
+def check_field_length(field_name: str, max_length: int):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            res = f(*args, **kwargs):
+            if len(res) > max_length
