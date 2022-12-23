5dc8893488600920b97dbef36e2e58cbc04b7850
VasYliev1980 <Yura.Vasilev1980@yandex.ru>
2022-12-23 19:42:10 +0300

    add.main.py

--- main.py
+++ main.py
@@ -23,4 +23,31 @@ def get_title() -> str: # Функция получения поля title
     return linecache.getline("books.txt", random.randint(1, 5)).strip()
 
 
+def get_year() -> int: # Функция получения поля Year
+    return random.randint(1900, datetime.datetime.now().year)
 
+def get_pages() -> int: # функция получения поля pages
+    return random.randint(0,10000)
+
+def get_isbn13() -> str:  # Функция получения поля isbn13
+    return fake.isbn13()
+
+def get_raiting() -> float: # Функция получения поля raiting
+    return random.uniform(0, 5)
+
+def get_price() -> float: # Функция получения поля price
+    return random.uniform(0, 10000)
+
+def get_authors() -> list[str]: # Функция получения поля authors
+    return list(fake.name() for _ in range(random.randint(1, 3)))
+
+def gen_dir(num:int = 1) -> dir: # Функция-генератор словарей
+    pk = num
+    while True:
+        yield {
+            "model": conf.MODEL,
+            "pk":pk,
+            "fields":{
+                "title":
+            }
+        }
